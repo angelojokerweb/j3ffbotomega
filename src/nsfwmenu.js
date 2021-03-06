e22a0920f964d0721048a03fa1418120921fa1f0
angelojokerweb <79103848+angelojokerweb@users.noreply.github.com>
2021-03-06 07:15:51 -0300

    Add files via upload

---
+++ src/nsfwmenu.js
@@ -0,0 +1,22 @@
+const nsfwmenu = (prefix, pushname) => {
+    return `𝘀𝗹𝘃 resumindo aqui e o menu NSFW
+    ◪ *NSFW*
+  │
+  ├─ ❏ ${prefix}nsfwbobs
+  ├─ ❏ ${prefix}randomhentaio
+  ├─ ❏ ${prefix}nsfwtrap
+  ├─ ❏ ${prefix}nsfwass
+  ├─ ❏ ${prefix}nsfwbelly
+  ├─ ❏ ${prefix}nsfwsidebobs
+  ├─ ❏ ${prefix}nsfwahegao
+  ├─ ❏ ${prefix}nsfwthighs
+  ├─ ❏ ${prefix}nsfwarmpits
+  └─ ❏ ${prefix}nsfwfeets
+
+  _obs para usar esses comandos ative o menu NSFW_\n _Digite_\n ${prefix}*nsfw 1*`
+
+
+
+}
+
+exports.nsfwmenu = nsfwmenu
---
+++ src/paulomenu.js
@@ -0,0 +1,12 @@
+const paulomenu = (prefix, pushname) => {
+    return `◪ *Comandos do J3FF 𝗕𝗢𝗧*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.paulomenu = paulomenu
